91b7b7e27fdadde5b2a0b56cf04befdfa09e74d4
Gleb_Kostenko <glebkostenko35@gmail.com>
2020-12-03 19:08:50 +0300

    third try

--- src/com/foxminded/DoAnagram.java
+++ src/com/foxminded/DoAnagram.java
@@ -3,9 +3,8 @@ package com.foxminded;
 import java.util.Stack;
 
 public class DoAnagram {
-    DoAnagram(){}
-
     public static String DoAnagramFromWord(String word){
+
         char[] symbols_of_word = word.toCharArray();
         Stack<Character> reverse_letter_word = new Stack<Character>();
 
@@ -14,7 +13,6 @@ public class DoAnagram {
                 reverse_letter_word.add(word.charAt(i));
             }
         }
-
 
         for(int i = 0;i < symbols_of_word.length;++i){
             if(Character.isLetter(symbols_of_word[i])){
@@ -26,10 +24,13 @@ public class DoAnagram {
         return anagram;
     }
 
-    public static String[] DoAnagramFromText(String[] words){
+    public static String DoAnagramFromText(String text){
+        String reversed_text = "";
+        String[] words = text.split("\\s");
         for (int i = 0;i < words.length;++i) {
             words[i] = DoAnagramFromWord(words[i]);
+            reversed_text = reversed_text + words[i] + " ";
         }
-        return words;
+        return reversed_text;
     }
 }
--- src/com/foxminded/Main.java
+++ src/com/foxminded/Main.java
@@ -8,11 +8,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan_text = new Scanner(System.in);
         String text = scan_text.nextLine();
-        String[] words = text.split("\\s");
-        words = DoAnagram.DoAnagramFromText(words);
-        for (String word : words) {
-            System.out.println(word);
-        }
+        System.out.print(DoAnagram.DoAnagramFromText(text));
     }
-
 }
